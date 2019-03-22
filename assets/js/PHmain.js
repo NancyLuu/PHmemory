593dc709e24087adab436568e5a9a26641d7c8d8
Nancy <NancyLu@Nancyde-MacBook-Air.local>
2019-03-22 21:19:31 +0800

    Add all about filelist pages

--- assets/js/PHmain.js
+++ assets/js/PHmain.js
@@ -9,76 +9,31 @@ function initMap() {
 
 $(document).ready(function () {
 
-    $("#btn_item_video").click(function () {
-        var lable = $("#btn_item_video").text().trim();
-        if (lable == "▼") {
-            $("#btn_item_video").text("◄");
-            $(".item_video").hide();
-        }
-        else {
-            $("#btn_item_video").text("▼");
-            $(".item_video").show();
-        }
-    });
+    $(document).ready(function () {
 
-    $("#btn_item_oranization").click(function () {
-        var lable = $("#btn_item_oranization").text().trim();
-        if (lable == "▼") {
-            $("#btn_item_oranization").text("◄");
-            $(".item_oranization").hide();
-        }
-        else {
-            $("#btn_item_oranization").text("▼");
-            $(".item_oranization").show();
-        }
-    });
+        function shown() {
+            var lable = $(this).text().trim();
+            var name = $(this).get(0).id;
+            if (lable == "展開") {
+                $(this).text("隱藏");
+                $('.'+name).show();
+            }
+            else {
+                $(this).text("展開");
+                $('.'+name).hide();
+            }
+        };
 
-    $("#btn_item_space").click(function () {
-        var lable = $("#btn_item_space").text().trim();
-        if (lable == "▼") {
-            $("#btn_item_space").text("◄");
-            $(".item_space").hide();
-        }
-        else {
-            $("#btn_item_space").text("▼");
-            $(".item_space").show();
-        }
-    });
-
-    $("#btn_item_works").click(function () {
-        var lable = $("#btn_item_works").text().trim();
-        if (lable == "▼") {
-            $("#btn_item_works").text("◄");
-            $(".item_works").hide();
-        }
-        else {
-            $("#btn_item_works").text("▼");
-            $(".item_works").show();
-        }
-    });
-
-    $("#btn_item_people").click(function () {
-        var lable = $("#btn_item_people").text().trim();
-        if (lable == "▼") {
-            $("#btn_item_people").text("◄");
-            $(".item_people").hide();
-        }
-        else {
-            $("#btn_item_people").text("▼");
-            $(".item_people").show();
-        }
-    });
-
-    $("#btn_item_activity").click(function () {
-        var lable = $("#btn_item_activity").text().trim();
-        if (lable == "▼") {
-            $("#btn_item_activity").text("◄");
-            $(".item_activity").hide();
-        }
-        else {
-            $("#btn_item_activity").text("▼");
-            $(".item_activity").show();
-        }
+        $("#groupList01").on('click', shown);
+        $("#groupList02").on('click', shown);
+        $("#groupList03").on('click', shown);
+        $("#groupList04").on('click', shown);
+        $("#groupList05").on('click', shown);
+        $("#groupList06").on('click', shown);
+        $("#groupList07").on('click', shown);
+        $("#groupList08").on('click', shown);
+        $("#groupList09").on('click', shown);
+        $("#groupList10").on('click', shown);
     });
     
 
